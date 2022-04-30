5262088cb53d67641cd6520e03097e3f2ef04f19
Артем Леванов <tema.levanov@gmail.com>
2022-04-30 18:22:02 +0300

    delete unnecessary message on start

--- src/pages/contacts/contacts.js
+++ src/pages/contacts/contacts.js
@@ -1,35 +1,33 @@
-import './contacts.scss';
+import "./contacts.scss";
 import TabsClass from "../../components/other/TabsClass";
 import DropdownBlockClass from "../../components/other/DropdownBlockClass";
 import SliderMain from "../../components/other/SliderMain";
 
 const Contacts = () => {
+  const tabs = document.querySelectorAll(".tabs");
 
-    const tabs = document.querySelectorAll('.tabs');
+  for (let i = 0; i < tabs.length; i++) {
+    new TabsClass(tabs[i]);
+  }
+  new DropdownBlockClass(".dropdown-block");
 
-    for (let i = 0; i < tabs.length; i++) {
-        new TabsClass(tabs[i]);
-    }
-    new DropdownBlockClass('.dropdown-block');
+  const slider = SliderMain({
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    slidesPerView: 2,
+    slidesPerGroup: 1,
+    spaceBetween: 20,
+  });
 
-    const slider = SliderMain({
-        pagination: {
-            el: ".swiper-pagination",
-            clickable: true,
-        },
-        navigation: {
-            nextEl: ".swiper-button-next",
-            prevEl: ".swiper-button-prev",
-        },
-        slidesPerView: 2,
-        slidesPerGroup: 1,
-        spaceBetween: 20,
-    });
-
-    slider.on('slideChange', function () {
-        console.log(222, 'doit')
-    })
-
-}
+  slider.on("slideChange", function () {
+    console.log(222, "doit");
+  });
+};
 
 Contacts();
