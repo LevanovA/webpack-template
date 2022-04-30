2548e83468f0a784ae2eef0d31dd3733a2250866
Артем Леванов <tema.levanov@gmail.com>
2022-04-30 21:32:12 +0300

    adds editor cong

--- src/components/other/DropDownBlockFunc/DropDownBlockFunc.js
+++ src/components/other/DropDownBlockFunc/DropDownBlockFunc.js
@@ -1,24 +1,25 @@
-import './DropDownBlockFunc.scss'
+import './DropDownBlockFunc.scss';
 
 const DropDownBlockFunc = () => {
+	const $dropdownsList = document.querySelectorAll('.dropdown-block');
 
-    const $dropdownsList = document.querySelectorAll('.dropdown-block');
+	$dropdownsList.forEach((list) => {
+		const $dropdownBox = list.querySelectorAll('.dropdown-block__box');
 
-    $dropdownsList.forEach(list => {
-        const $dropdownBox = list.querySelectorAll('.dropdown-block__box');
+		$dropdownBox.forEach((box) => {
+			box.addEventListener('click', changeVisibility);
+		});
+	});
 
-        $dropdownBox.forEach(box => {
-            box.addEventListener('click', changeVisibility)
-        })
-    })
+	function changeVisibility() {
+		const $dropdownItem = this.parentElement;
+		const $dropdownAnswer = $dropdownItem.querySelector(
+			'.dropdown-block__answer'
+		);
 
-    function changeVisibility() {
-        const $dropdownItem = this.parentElement;
-        const $dropdownAnswer = $dropdownItem.querySelector('.dropdown-block__answer');
+		this.classList.toggle('dropdown-block__box--active');
+		$dropdownAnswer.classList.toggle('dropdown-block__answer--active');
+	}
+};
 
-        this.classList.toggle('dropdown-block__box--active');
-        $dropdownAnswer.classList.toggle('dropdown-block__answer--active');
-    }
-}
-
-export default DropDownBlockFunc
+export default DropDownBlockFunc;
