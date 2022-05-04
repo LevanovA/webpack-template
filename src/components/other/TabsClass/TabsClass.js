f9dcf2efdab8bd39b2969b41acf576462ef63a43
Артем Леванов <tema.levanov@gmail.com>
2022-05-04 08:59:33 +0300

    config edits

--- src/components/other/TabsClass/TabsClass.js
+++ src/components/other/TabsClass/TabsClass.js
@@ -1,41 +1,40 @@
-import './TabsClass.scss'
+import './TabsClass.scss';
 
 export default class TabsClass {
-    constructor(tabs) {
-        this.$tab = tabs;
-        this.$tabName = this.$tab.querySelectorAll('.tabs__name');
-        this.$tabItem = this.$tab.querySelectorAll('.tabs__item');
+	constructor(tabs) {
+		this.$tab = tabs;
+		this.$tabName = this.$tab.querySelectorAll('.tabs__name');
+		this.$tabItem = this.$tab.querySelectorAll('.tabs__item');
 
-        this.init();
-    }
+		this.init();
+	}
 
-    init () {
-        this.$tabName.forEach((name, index) => {
-            name.setAttribute('data-tab-index', `${index}`)
-            name.addEventListener('click', this.changeTab.bind(this))
-        })
-    }
+	init() {
+		this.$tabName.forEach((name, index) => {
+			name.setAttribute('data-tab-index', `${index}`);
+			name.addEventListener('click', this.changeTab.bind(this));
+		});
+	}
 
-    changeTab (e) {
-        const targetTab = e.target.closest('.tabs__name');
-        const blockNumber = targetTab.getAttribute('data-tab-index')
+	changeTab(e) {
+		const targetTab = e.target.closest('.tabs__name');
+		const blockNumber = targetTab.getAttribute('data-tab-index');
 
-        this.$tabName.forEach(name => {
-            name.classList.remove('active');
-        })
+		this.$tabName.forEach((name) => {
+			name.classList.remove('active');
+		});
 
-        targetTab.classList.add('active');
+		targetTab.classList.add('active');
 
-        this.openTabItem(blockNumber)
+		this.openTabItem(blockNumber);
 
-        console.log(111, blockNumber)
-    }
+		console.log(111, blockNumber);
+	}
 
-    openTabItem (blockNumber) {
-        this.$tabItem.forEach(name => {
-            name.classList.remove('active');
-        })
-        this.$tabItem[blockNumber].classList.add('active')
-    }
-
+	openTabItem(blockNumber) {
+		this.$tabItem.forEach((name) => {
+			name.classList.remove('active');
+		});
+		this.$tabItem[blockNumber].classList.add('active');
+	}
 }
--- src/components/other/TabsClass/index.js
+++ src/components/other/TabsClass/index.js
@@ -1,1 +1,1 @@
-export { default } from './TabsClass'
+export { default } from './TabsClass';
